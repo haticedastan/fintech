1ac049d3c1da68dce0bb8a77714ba04f8a44ae14
Hatice <dstnhtc@hotmail.com>
2022-04-26 00:46:25 +0300

    axios api used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v2/all")
+      .then((response) => setCountries(response.data));
+  }, []);
   return (
     <div className="App">
-    
-
+      <h1>Ülke LİSTELERİ</h1>
+      {countries.map((country) => {
+        return (
+          <div key={country.name}>
+            <h2>{country.name}</h2>
+            <h4>{country.capital}</h4>
+            <p>
+              <img src={country.flag} alt={country.name}
+              style={{width:"100px"}}></img>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
